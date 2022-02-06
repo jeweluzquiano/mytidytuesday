f5131d8399ef79c2c9e3f4dcf88ee881eaefcc6c
Jewel Uzquiano <uzquianojewel@gmail.com>
2022-02-05 18:42:34 -0600

    joined data

--- 20220125_games.R
+++ 20220125_games.R
@@ -1,3 +1,8 @@
-install.packages("tidytuesdayR")
+# install.packages("tidytuesdayR")
 library(tidytuesdayR)
+library(tidyverse)
 tt_data <- tt_load(2022, week=4)
+ratings <- tt_data$ratings
+details <- tt_data$details
+View(ratings)
+overview <- full_join(details, ratings, by = "id")
